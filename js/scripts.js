b4a378dbbcea8d34b6acef2a7902f63e8955dc4b
kaya jepson and Marc Davies <marc@>
2019-04-03 15:14:14 -0700

    wut

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,22 @@ var translate = function(sentence) {
   //   }
 };
 
+function checkWord(word) {
+//pig laten word stuff
+  word += "Poop"
+  return word
+}
 
+function checkPhrase(phrase) {
+  var splitWords = phrase.split(" ");
+  var latinPhrase = "";
+  for (var i = 0; i < splitWords.length; i++) {
+    latinPhrase += checkWord(splitWords[i])
+    latinPhrase += " ";
+  }
+
+  return latinPhrase
+}
 
 
 
@@ -23,31 +38,39 @@ $(document).ready(function() {
     // var result = translate(sentence);
     var sentence = [];
     var firstThree = [];
-    $("input#words").each(function(){
-    var wordArray = $(this).val().split(" ");
 
-    // wordArray = (val);
-    sentence.push(wordArray);
-    console.log(sentence);
-    var char = sentence.slice(0,2);
-    firstThree.push(char);
-    console.log(firstThree);
-    // console.log(wordArray);
-    // console.log(char);
 
-  });
 
-  for (var index = 0; index < char.length; index ++) {
-    for(var i = 0; i < 3; i++){
-      var char = sentence.slice(0,2);
-      firstThree.push(char);
-      console.log(firstThree);
-    };
-    // var letterArray = $(this).val().split("",3);
-    // console.log(letterArray);
-  }
+    var latin = checkPhrase($("input#words").val());
+    console.log(latin);
+    $(".words").text(latin);
+    $("#output").show();
+    // $("input#words").each(function(){
 
-    $(".words").text(sentence);
+  //   var wordArray = $(this).val().split(" ");
+  //
+  //   // wordArray = (val);
+  //   sentence.push(wordArray);
+  //   console.log(sentence);
+  //   var char = sentence.slice(0,2);
+  //   firstThree.push(char);
+  //   console.log(firstThree);
+  //   // console.log(wordArray);
+  //   // console.log(char);
+  //
+  // });
+  //
+  // for (var index = 0; index < char.length; index ++) {
+  //   for(var i = 0; i < 3; i++){
+  //     var char = sentence.slice(0,2);
+  //     firstThree.push(char);
+  //     console.log(firstThree);
+  //   };
+  //   // var letterArray = $(this).val().split("",3);
+  //   // console.log(letterArray);
+  // }
+  //
+  //   $(".words").text(sentence);
 
 
 
